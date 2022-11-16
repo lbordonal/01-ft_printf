b06649d26234ba608a6d05256c0360705c7b93b3
Lucas Henrique Bordonal <lbordona@c1r9s1.42porto.com>
2022-11-16 11:50:52 +0000

    16/11/22 - 11:50

---
+++ ft_printf/libftprintf.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbordona <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/16 11:42:59 by lbordona          #+#    #+#             */
+/*   Updated: 2022/11/16 11:46:59 by lbordona         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <string.h>
+# include <strings.h>
+# include <stddef.h>
+# include <ctype.h>
+# include <stdarg.h>
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
+
+#endif
